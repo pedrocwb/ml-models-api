8b887b151b0ff07a6c2d469ba307b43380a25a76
Pedro Martins <pedromartins.cwb@gmail.com>
2023-04-20 12:03:53 -0300

    Add logs to the model

--- models/iris_model/src/api.py
+++ models/iris_model/src/api.py
@@ -1,7 +1,8 @@
+import sys
 import os
 from typing import Any, List
 from pathlib import Path
-
+import logging
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -9,6 +10,10 @@ from src.model import IrisModel
 
 ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format='[%(levelname)s] %(message)s')
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -23,8 +28,12 @@ class PredictionResponse(BaseModel):
 
 @app.post("/predict", response_model=PredictionResponse)
 async def predict(request: PredictionRequest):
+    logger.info(f"Incoming request: {request.input_data}")
+
     model = IrisModel(f"{ROOT_DIR}/assets/iris_knn.joblib")
     prediction = model.predict(request.input_data)
+
+    logger.info(f"Prediction response: {prediction}")
     return {"prediction": prediction}
 
 
